1f7c79482c3c51af51028ae0afde77d93bd04197
Andy Zimolzak (work HP laptop) <zimolzak@users.noreply.github.com>
2022-09-23 14:14:33 -0500

    Tried 1 join but it still takes long

--- find-order-comments.sql
+++ find-order-comments.sql
@@ -32,8 +32,8 @@ select top 14
       ,[Sta3n]
       ,[PatientSID]
       ,[OrderStartDateTime]
-      ,[OrderActionDateTime]
-      ,[OrderAction]
+      ,[OrderActionDateTime]  -- seems boring
+      ,[OrderAction]  -- seems boring
       ,[ProviderSID]
 
       ,[TextReference]
@@ -44,3 +44,51 @@ select top 14
 
 from CDWWork.CPRSOrder.OrderAction
 where OrderStartDateTime > '2022-08-01'
+  and sta3n = 580
+  --and OrderText is not null
+
+/* how do I get to orderableItem? */
+
+select * from INFORMATION_SCHEMA.COLUMNS
+where TABLE_SCHEMA = 'cprsorder'
+  and COLUMN_NAME = 'orderableItemSID'
+-- OrderedItem is the only table that linkes to OrderableItem
+
+select * from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME = 'cprsorder'
+  and COLUMN_NAME like '%SID'
+
+-- orderAction can link back to CPRSOrder.
+-- orderedItem also links back to CPRSOrder.
+
+
+/**** trying multi join ********/
+
+select top 15 * 
+into #az_top15ord
+from [CDWWork].[CPRSOrder].[CPRSOrder]
+where sta3n = 580 and PatientSID > 0 and orderstartDatetime > '2010-01-01'
+
+SELECT 
+c.[CPRSOrderSID],
+c.[Sta3n],
+c.[PatientSID],
+c.[EnteredDateTime],
+c.[OrderType],
+c.[InPatientFlag],
+c.[OrderStartDateTime],
+
+      a.[ProviderSID],
+
+      a.[TextReference],
+
+      a.[OrderText],
+      a.[ExternalText],
+      a.[WardComments]
+
+
+  FROM #az_top15ord as c
+  -- where sta3n = 580 and PatientSID > 0 and orderstartDatetime > '2010-01-01'
+left join CDWWork.CPRSOrder.OrderAction as a
+on c.CPRSOrderSID = a.CPRSOrderSID
+
